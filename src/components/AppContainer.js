f1bb2d30e1e91bdeb4b450de15705a4f26c2d2c5
Jeremy Lenz <jeremylenz@gmail.com>
2018-10-05 11:11:24 -0400

    AppContainer basic build

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,14 +1,22 @@
 import React, { Component, Fragment } from 'react';
-import { Divider } from 'semantic-ui-react'
+import { Grid, Header } from 'semantic-ui-react'
 
 class AppContainer extends Component {
 
   render() {
     return (
       <Fragment>
-        <div>ChatWindow A</div>
-        <Divider vertical />
-        <div>ChatWindow B</div>
+        <Header as='h1'>Rob and Laura's BoxyChat</Header>
+        <Grid columns={2} celled>
+          <Grid.Row>
+            <Grid.Column>
+              <div className="left-chat">ChatWindow A</div>
+            </Grid.Column>
+            <Grid.Column>
+              <div className="right-chat">ChatWindow B</div>
+            </Grid.Column>
+        </Grid.Row>
+        </Grid>
       </Fragment>
     );
   }
